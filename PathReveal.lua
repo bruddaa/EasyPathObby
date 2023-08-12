0df1a668775225b837bf6f127f3dae0b4202d7f0
Brudda <139983537+bruddaa@users.noreply.github.com>
2023-08-13 00:10:28 +0200

    Create PathReveal.lua

---
+++ PathReveal.lua
@@ -0,0 +1,9 @@
+-- Made by Brudda
+-- github.com/bruddaa
+-- dsc.gg/join-brudda
+
+local v0=string.char;local v1=string.byte;local v2=string.sub;local v3=bit32 or bit ;local v4=v3.bxor;local v5=table.concat;local v6=table.insert;local function v7(v10,v11)local v12={};for v14=1, #v10 do v6(v12,v0(v4(v1(v2(v10,v14,v14 + 1 )),v1(v2(v11,1 + (v14% #v11) ,1 + (v14% #v11) + 1 )))%256 ));end return v5(v12);end local function v8(v13)for v15,v16 in ipairs(v13:GetChildren()) do if v16:IsA(v7("\247\204\215\33\227\169","\126\177\163\187\69\134\219\167")) then local v17=0 -0 ;local v18;local v19;local v20;while true do if (v17==(1892 -(1242 + 649))) then v20=nil;while true do if (v18==(2 -1)) then while true do if (v19==(0 -0)) then v20=v16:FindFirstChild(v7("\23\194\63\198\244\10\195\62\192\238\38\222\62","\156\67\173\74\165"));for v21,v22 in ipairs(v16:GetChildren()) do if v22:IsA(v7("\4\182\91\2","\38\84\215\41\118\220\70")) then local v23= #v22:GetChildren()>(0 + 0) ;if v23 then local v24=997 -(915 + 82) ;local v25;while true do if (v24==(0 + 0)) then v25=0 + 0 ;while true do if (v25==(350 -(87 + 263))) then v22.BrickColor=BrickColor.new(Color3.new(1 -0 ,180 -(67 + 113) ,0));v22.Transparency=0.5;break;end end break;end end else v22.BrickColor=BrickColor.new(Color3.new(0 -0 ,1 + 0 ,0 -0 ));v22.Material=Enum.Material.Neon;end end end break;end end break;end if (0==v18) then v19=0 -0 ;v20=nil;v18=1 + 0 ;end end break;end if ((0 -0)==v17) then v18=0 + 0 ;v19=nil;v17=792 -(368 + 423) ;end end end end end local v9=workspace:FindFirstChild(v7("\99\2\35\21\251\67","\158\48\118\66\114"));if v9 then v8(v9);end
+
+-- Made by Brudda
+-- github.com/bruddaa
+-- dsc.gg/join-brudda
